54e50857938f8376a446d381f17c252b783b2bcd
ushivada-tech <ushivada@gmail.com>
2026-01-31 11:49:13 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sum;
+printf("enter the first number:\n");
+scanf("%d",&a);
+printf("enter the second number:\n");
+scanf("%d",&b);
+sum=a+b;
+printf("sum is :%d",sum);
+}
+
